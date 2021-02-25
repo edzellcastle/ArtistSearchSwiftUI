3be7d24e4f1c9281f22cef4138daccc48930b1d5
David Lindsay <davidlindsay100@icloud.com>
2021-02-24 21:11:08 -0800

    Initial Commit

---
+++ ArtistSearchSwiftUI/ArtistSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistSearchSwiftUIApp.swift
+//  ArtistSearchSwiftUI
+//
+//  Created by David Lindsay on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ArtistSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
